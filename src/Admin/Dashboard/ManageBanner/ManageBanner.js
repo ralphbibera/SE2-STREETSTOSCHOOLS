b307d4423bd8837534794217a16481db841a7f8a
ralphbibera <98459325+ralphbibera@users.noreply.github.com>
2022-03-06 19:54:20 +0800

    Added Records

--- src/Admin/Dashboard/ManageBanner/ManageBanner.js
+++ src/Admin/Dashboard/ManageBanner/ManageBanner.js
@@ -1,117 +1,71 @@
 import "../../Stylesheets/table.css";
-import { Button, Col, Form, ProgressBar } from "react-bootstrap";
-import { useState } from "react";
-import { toast } from "react-toastify";
-import { useDispatch } from "react-redux";
-import { doBanners } from "../../../redux/actionCreators/bannerActionCreators";
+import { deleteBanner } from "../../../redux/actionCreators/bannerActionCreators";
 import { useCollectionData } from "react-firebase-hooks/firestore";
 import { firestore } from "../../../config/firebase";
+import AddBanner from "./AddBanner";
+import { Switch, Route, useRouteMatch, Link } from "react-router-dom";
 
 const ManageBanner = () => {
-  const [title, setTitle] = useState("");
-  const [image, setImage] = useState("");
-  const [progress, setProgress] = useState(0);
-  const dispatch = useDispatch();
+  const { path } = useRouteMatch();
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-
-    if (!title) {
-      return toast.warning("Please fill all fields");
-    }
-
-    if (!image || image === undefined) {
-      return toast.warning("Please select an image");
-    }
-
-    if (title.trim().split(" ").length < 2) {
-      return toast.warning("Title at least 2 words");
-    }
-
-    if (image.size > 5242880) {
-      return toast.warning("Max 5MB FOR IMAGE");
-    }
-
-    const data = {
-      title: title,
-      image: "",
-      date: new Date(),
-    };
-    
-    dispatch(doBanners(data, image, setProgress));
-  };
-  
   const banners = firestore.collection("banners");
-  const bannerQuery = banners.limit(30).orderBy("date","asc");
-  const [bannerList] = useCollectionData(bannerQuery, {idField: 'id'});
+  const bannerQuery = banners.limit(30).orderBy("date", "asc");
+  const [bannerList] = useCollectionData(bannerQuery, { idField: "id" });
 
-  
   return (
     <div>
-      <Col className="mx-auto">
-        {progress > 0 && progress < 100 ? (
-          <>
-            <h1>Uploading Post {progress} %</h1>
-            <ProgressBar now={progress} max={100} />
-          </>
-        ) : progress === 100 ? (
-          <>
-            <h1>Post Uploaded Successfully </h1>
-          </>
-        ) : (
-          <Form onSubmit={handleSubmit} className="p-4">
-            <Form.Group controlId="name" className="my-2">
-              <Form.Control
-                type="text"
-                name="title"
-                placeholder="Title"
-                value={title}
-                onChange={(e) => setTitle(e.target.value)}
-              />
-            </Form.Group>
-            <Form.Group controlId="file" className="my-2">
-              <Form.Control
-                type="file"
-                name="image"
-                onChange={(e) => setImage(e.target.files[0])}
-              />
-            </Form.Group>
-            <Form.Group controlId="btn" className="my-2">
-              <Button
-                type="submit"
-                variant={"dark"}
-                bg="dark"
-                className="form-control"
-              >
-                Add Banner
-              </Button>
-            </Form.Group>
-          </Form>
-        )}
-      </Col>
-
-      {bannerList ? (
-        bannerList && bannerList.map((banners, index) => (
-          <table key={index}>
-            <tbody style={{ fontsize: "15px" }}>
-              <tr className="">
-                <td>{index + 1}</td>
-                <td style={{width: "600px"}}>
-                  <img id="banner-img" src={banners.image} />
-                </td>
-                <td style={{width: "350px"}}>{banners.title}</td>
-                <td className="">
-                  <Button className="action-buttons edit-btn">Edit</Button>
-                  <Button className="action-buttons delete-btn ">Archive</Button>
-                </td>
-              </tr>
-            </tbody>
-          </table>
-        ))
-      ) 
-      : (
-        <>Loading</>
-      ) }
+      <Switch>
+        <Route exact path={path}>
+          <div className="top-buttons container d-flex justify-content-between">
+            <Link to="/admin/dashboard/manage-banner/addbanner">
+              <button>New Banner</button>
+            </Link>
+          </div>
+          <div className="container">
+            <table>
+              <thead>
+                <tr>
+                  <th>ID</th>
+                  <th>IMAGE</th>
+                  <th>TITLE</th>
+                  <th colSpan={2}>ACTIONS</th>
+                </tr>
+              </thead>
+              {bannerList ? (
+                bannerList &&
+                bannerList.map((banners, index) => (
+                  <tbody key={index}>
+                    <tr>
+                      <td>{index + 1}</td>
+                      <td style={{ width: "600px" }}>
+                        <img id="banner-img" src={banners.image} alt="Banner" />
+                      </td>
+                      <td style={{ width: "350px" }}>{banners.title}</td>
+                      <td>
+                        <button
+                          className="action-buttons delete-btn"
+                          onClickCapture={() => deleteBanner(banners.id)}
+                        >
+                          Delete
+                        </button>
+                      </td>
+                    </tr>
+                  </tbody>
+                ))
+              ) : (
+                <tbody>
+                  <tr>
+                    <td>Loading</td>
+                  </tr>
+                </tbody>
+              )}
+            </table>
+          </div>
+        </Route>
+        <Route path={path + "/addbanner"}>
+          <AddBanner />
+        </Route>
+      </Switch>
     </div>
   );
 };
--- src/Admin/Dashboard/ManageRecords/ManageRecords.js
+++ src/Admin/Dashboard/ManageRecords/ManageRecords.js
@@ -1,18 +1,44 @@
 import { Switch, Route, useRouteMatch, Link } from "react-router-dom";
-import { useHistory } from "react-router-dom";
 import { useCollectionData } from "react-firebase-hooks/firestore";
 import { firestore } from "../../../config/firebase";
 import AddRecords from "./AddRecords";
-import { Button } from "react-bootstrap";
 import "../../Stylesheets/table.css";
+import { deleteRecord } from "../../../redux/actionCreators/recordActionCreator";
 
 const ManageRecords = () => {
   const { path } = useRouteMatch();
-  const history = useHistory();
 
   const record = firestore.collection("records");
-  const recordQuery = record.limit(50).orderBy("date", "asc");
+  const recordQuery = record.limit(50).orderBy("donation_date", "desc");
   const [recordList] = useCollectionData(recordQuery);
+
+  const createPDF = () => {
+    var sTable = document.getElementById("tab").innerHTML;
+
+    var style = "<style>";
+    style = style + "table {width: 100%;font: 18px Calibri;}";
+    style =
+      style +
+      "table, th, td {border: solid 1px #DDD; border-collapse: collapse;";
+    style = style + "padding: 2px 3px;text-align: center;}";
+    style = style + "#hideme {display: none !important;}";
+    style = style + "</style>";
+
+    // CREATE A WINDOW OBJECT.
+    var win = window.open("", "", "height=700,width=700");
+
+    win.document.write("<html><head>");
+    win.document.write("<title>Records</title>"); // <title> FOR PDF HEADER.
+    win.document.write(style); // ADD STYLE INSIDE THE HEAD TAG.
+    win.document.write("</head>");
+    win.document.write("<body>");
+    win.document.write(sTable); // THE TABLE CONTENTS INSIDE THE BODY TAG.
+    win.document.write("</body></html>");
+
+    win.document.close(); // CLOSE THE CURRENT WINDOW.
+
+    win.print(); // PRINT THE CONTENTS.
+  };
 
   return (
     <div className="">
@@ -22,9 +48,9 @@ const ManageRecords = () => {
             <Link to="/admin/dashboard/manage-records/addrecord">
               <button>New Records</button>
             </Link>
-            <button>Print Records</button>
+            <button onClick={() => createPDF()}>Print Records</button>
           </div>
-          <div className="container">
+          <div className="container" id="tab">
             <table>
               <thead>
                 <tr>
@@ -32,27 +58,36 @@ const ManageRecords = () => {
                   <th>NAME</th>
                   <th>PLATFORM</th>
                   <th>AMOUNT</th>
-                  <th colSpan={2}>ACTIONS</th>
+                  <th colSpan={2} id="hideme">
+                    ACTIONS
+                  </th>
                 </tr>
               </thead>
               {recordList ? (
                 recordList &&
-                recordList.map((record,index) => (
-                  <tbody>
-                    <tr key={index}>
-                      <td>{record.donation_date}</td>
+                recordList.map((record, index) => (
+                  <tbody key={index} id="table-content">
+                    <tr>
+                      <td>
+                        {new Date(
+                          record.donation_date.seconds * 1000
+                        ).toLocaleDateString()}
+                      </td>
                       <td>{record.donor}</td>
                       <td>{record.platform}</td>
                       <td>{record.amount}</td>
-                      <td>
-                        <Button className="action-buttons edit-btn">
+                      <td id="hideme">
+                        <button className="action-buttons edit-btn">
                           Edit
-                        </Button>
+                        </button>
                       </td>
-                      <td>
-                        <Button className="action-buttons delete-btn">
+                      <td id="hideme">
+                        <button
+                          className="action-buttons delete-btn"
+                          onClick={() => deleteRecord(record.id)}
+                        >
                           Delete
-                        </Button>
+                        </button>
                       </td>
                     </tr>
                   </tbody>
